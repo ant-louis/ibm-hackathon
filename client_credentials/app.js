3be4205fe75ebd836d65d5421c583a4c66a26889
arcticdreamz <tomcrasset@gmail.com>
2018-11-23 20:17:11 +0100

    Use request-promise package to handle asynchronous code

--- client_credentials/app.js
+++ client_credentials/app.js
@@ -14,13 +14,14 @@ limitations under the License.
 */
 
 
-var request = require('request'); // "Request" library
+var rp = require('request-promise'); // "Request" library
 
 var client_id = 'd171b87bddde4f62b14525463e1bb3f1'; // Your client id
 var client_secret = '956fbe6cddef4cfa80bfcd2d0f879712'; // Your secret
 
 // your application requests authorization
 var authOptions = {
+  method: 'POST',
   url: 'https://accounts.spotify.com/api/token',
   headers: {
     'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
@@ -28,47 +29,122 @@ var authOptions = {
   form: {
     grant_type: 'client_credentials'
   },
-  json: true
+  json: true,
+  resolveWithFullResponse: true
 };
 
 
-request.post(authOptions, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-
+// function getTrack(options){
+//   var tracks = [];
+//   var track_links = []
+//   var track_features = [];
+//   var maxtracks = 10000;
+//   var nbtracks_curr = 0;
+//   return Promise.all(
+//     request.get(options, function(error, response, body) {
+//         playlists = body.playlists.items;
+//         nextPlaylists = body.playlists.next; //Limit of 50
+//         // Iterate over all playlists (limit 50)
+//       for (let i = 0; i < playlists.length; i++) {
+        
+//         owner = playlists[i].owner.display_name;
+//         //Only take Spotify playlists
+//         if(owner == "Spotify"){ 
+//           track_links.push(playlists[i].tracks.href);
+//           nbtracks = playlists[i].tracks.total;
+//           nbtracks_curr += nbtracks;
+//         }
+//       }
+//       console.log(nbtracks_curr);
+//       })
+//    )
+// }
+rp(authOptions)
+.then(response => {
+  console.log(response.statusCode)
+  if (response.statusCode === 200) {
+    return response.body
+  }
+}).then(body => {
     // use the access token to access the Spotify Web API
     var token = body.access_token;
     var options = {
-
+      method: 'GET',
       url: 'https://api.spotify.com/v1/search?q=Spotify&type=playlist&offset=0&limit=50',
       headers: {
         'Authorization': 'Bearer ' + token
       },
-      json: true
+      json: true,
+      resolveWithFullResponse: true
     };
-    var next;
-    var tracks = [];
-    var track_links = []
-    var track_features = [];
-    var maxtracks = 10000;
-    var nbtracks_curr = 0;
-    //while(nbtrack_curr < maxtracks)
-
-    request.get(options, function(error, response, body) {
-        playlists = body.playlists.items;
-        nextPlaylists = body.playlists.next; //Limit of 50
-        // Iterate over all playlists (limit 50)
-      for (let i = 0; i < playlists.length; i++) {
-        
-        owner = playlists[i].owner.display_name;
-        //Only take Spotify playlists
-        if(owner == "Spotify"){ 
-          track_links.push(playlists[i].tracks.href);
-          nbtracks = playlists[i].tracks.total;
-          nbtracks_curr += nbtracks;
-        }
-      }
-      console.log(track_links)
-      console.log(nbtracks_curr);
-    })
+    return rp(options)
+})
+.then(response => {
+  console.log(response.statusCode)
+  if (response.statusCode === 200) {
+    return response.body
   }
-});
+})
+.then(body => {
+  console.log(body)
+})
+.catch(error => console.log(error))
+
+    // // use the access token to access the Spotify Web API
+    // var token = body.access_token;
+    // var options = {
+
+    //   url: 'https://api.spotify.com/v1/search?q=Spotify&type=playlist&offset=0&limit=50',
+    //   headers: {
+    //     'Authorization': 'Bearer ' + token
+    //   },
+    //   json: true
+    // };
+    // var next;
+
+    //while(nbtrack_curr < maxtracks)
+    
+
+//   }
+// });
+
+// request.post(authOptions, function(error, response, body) {
+//   if (!error && response.statusCode === 200) {
+
+//     // use the access token to access the Spotify Web API
+//     var token = body.access_token;
+//     var options = {
+
+//       url: 'https://api.spotify.com/v1/search?q=Spotify&type=playlist&offset=0&limit=50',
+//       headers: {
+//         'Authorization': 'Bearer ' + token
+//       },
+//       json: true
+//     };
+//     var next;
+//     var tracks = [];
+//     var track_links = []
+//     var track_features = [];
+//     var maxtracks = 10000;
+//     var nbtracks_curr = 0;
+//     //while(nbtrack_curr < maxtracks)
+
+//     request.get(options, function(error, response, body) {
+//         playlists = body.playlists.items;
+//         nextPlaylists = body.playlists.next; //Limit of 50
+//         // Iterate over all playlists (limit 50)
+//       for (let i = 0; i < playlists.length; i++) {
+        
+//         owner = playlists[i].owner.display_name;
+//         //Only take Spotify playlists
+//         if(owner == "Spotify"){ 
+//           track_links.push(playlists[i].tracks.href);
+//           nbtracks = playlists[i].tracks.total;
+//           nbtracks_curr += nbtracks;
+//         }
+//       }
+//       console.log(track_links)
+//       console.log(nbtracks_curr);
+//     })
+//   }
+// });
