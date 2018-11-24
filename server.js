bb69a30d418200ed7b322772c729407b093509b4
arcticdreamz <tomcrasset@gmail.com>
2018-11-24 17:00:36 +0100

    Put every file in / folder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var path = require("path");
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile(path.join(__dirname+'/index.php'));
   //__dirname : It will resolve to your project folder.
 });
 app.listen(3000);
